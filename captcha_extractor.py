d6430283afc9d9930abd3e52652054d2464dd639
shubham chauhan <41683500+chauhan01@users.noreply.github.com>
2020-06-16 14:10:34 +0530

    Update captcha_extractor.py

--- captcha_extractor.py
+++ captcha_extractor.py
@@ -3,16 +3,16 @@ import cv2
 import numpy as np
 import os
 import glob
-#load the model
 from keras.models import load_model
 import pickle
+from preprocessing import *
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
 	help="path to input image directory")
-ap.add_argument("-o", "--output", required=True,
-	help="path to output image")
+ap.add_argument("-save", "--save_solved", required=True,
+	help="True if you want to save the solved output")
 ap.add_argument("-m", "--model", required=True,
 	help="path to model.hdf5 file")
 ap.add_argument("-l", "--labels", required=True,
@@ -20,10 +20,15 @@ ap.add_argument("-l", "--labels", required=True,
 args = vars(ap.parse_args())
 
 test_image_folder = args['input']
-OUTPUT_FOLDER = args['output']
+OUTPUT_FOLDER_DETECTED = 'detected output'
+OUTPUT_FOLDER_SOLVED = 'solved captcha output'
+
+# if output directory does not exist creat one
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 LB = args['labels']
 MODEL = args['model']
-
+save_solved = args['save_solved']
 test_images = glob.glob(os.path.join(test_image_folder, "*"))
 
 #Loading the model
@@ -32,48 +37,27 @@ model = load_model(MODEL)
 #load labels file
 lb = open(LB, "rb")
 lb = pickle.load(lb)
-
 # loop over the image paths
 for image in test_images:
     # Load the image and convert it to grayscale
-    image = cv2.imread(image, cv2.IMREAD_COLOR)
+    orignal_image = cv2.imread(image, cv2.IMREAD_COLOR)
 
     # Add some extra padding around the image
-    image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+    image = cv2.copyMakeBorder(orignal_image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=(255, 255, 255))
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    median = cv2.medianBlur(gray, 1)
-
-    # applying threshold
-    thresh = cv2.threshold(median, 127, 255, cv2.THRESH_BINARY_INV, cv2.THRESH_OTSU)[1]
-
-    # applying erode
-    kernel = np.ones((2, 2), np.uint8)
-    thresh = cv2.erode(thresh, kernel, iterations=1)
-
-    # find all your connected components (white blobs in your image)
-    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity=8)
-    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
-    # the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
-    sizes = stats[1:, -1];
-    nb_components = nb_components - 1
-
-    # minimum size of particles we want to keep (number of pixels)
-    # here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
-    min_size = 150
-
-    # your answer image
-    img = np.zeros((output.shape))
-    # for every component in the image, you keep it only if it's above min_size
-    for i in range(0, nb_components):
-        if sizes[i] >= min_size:
-            img[output == i + 1] = 255
-            img = np.uint8(img)
-
-    # finding the contours
-    contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    img1, contours1 = image_process_1(image)
+    img2, contours2 = image_process_2(image)
+    img3, contours3 = image_process_3(image)
+    
+    if len(contours1) ==6:
+        img = img1
+        contours = contours1
+    elif len(contours2) ==6:
+        img = img2
+        contours = contours2       
+    else:
+        img = img3
+        contours = contours3
     # sorting contours
     cnts = sorted(contours, key=cv2.contourArea, reverse=True)
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
@@ -113,11 +97,12 @@ for image in test_images:
         # Extract the letter from the original image with a 2-pixel margin around the edge
         letter_image = out[y - 2:y + h + 2, x - 2:x + w + 2]
 
-        letter_image = cv2.resize(letter_image, (30, 30))
+        letter_image = cv2.resize(letter_image, (35, 35))
 
         # Turn the single image into a 4d list of images
         letter_image = np.expand_dims(letter_image, axis=2)
         letter_image = np.expand_dims(letter_image, axis=0)
+        letter_image = letter_image/255.0
 
         # making prediction
         pred = model.predict(letter_image)
@@ -132,10 +117,13 @@ for image in test_images:
 
     # Print the captcha's text
     captcha_text = "".join(predictions)
-    #     print("CAPTCHA text is: {}".format(captcha_text))
 
-    save_path = os.path.join(OUTPUT_FOLDER, captcha_text)
+    save_path_detected = os.path.join(OUTPUT_FOLDER_DETECTED, captcha_text)
+    detected_path = os.path.join(save_path_detected + '.png')
+    cv2.imwrite(detected_path, image)
 
-    p = os.path.join(save_path + '.png')
-
-    cv2.imwrite(p, image)
+    if save_solved = True:
+        save_path_solved = os.path.join(OUTPUT_FOLDER_SOLVED, captcha_text)
+        solved_path = os.path.join(save_path_solved + '.png')
+        cv2.imwrite(solved_path, orignal_image)
+    else: Pass
